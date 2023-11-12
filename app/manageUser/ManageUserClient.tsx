6c8cace0db371160b033571f2ef44c3d73c3d7e3
Harry <danghuuphuc001@gmail.com>
2023-11-12 22:13:03 +0700

    update get id

--- app/manageUser/ManageUserClient.tsx
+++ app/manageUser/ManageUserClient.tsx
@@ -194,7 +194,7 @@ const ManageUserClient: React.FC<ManageUserClientProps> = ({
               return (
                 <UserInfoListing
                   key={item.applicationUserTableData.id}
-                  id={item.applicationUserTableData.id}
+                  id={item.studentTableData.id}
                   fullName={item.applicationUserTableData.fullName}
                   email={item.applicationUserTableData.email}
                   date={item.studentTableData.created}
