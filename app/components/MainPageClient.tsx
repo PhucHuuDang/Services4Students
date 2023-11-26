3933af070d4ee66a8f34e0184aead208c3bece7a
Harry <danghuuphuc001@gmail.com>
2023-11-27 04:18:15 +0700

    get params from payment

--- app/components/MainPageClient.tsx
+++ app/components/MainPageClient.tsx
@@ -2,6 +2,9 @@
 
 import { toast } from "react-hot-toast";
 import { BsCartCheck } from "react-icons/bs";
+import Image from "next/image";
+import { GoHeartFill } from "react-icons/go";
+import { RiEmotionSadLine } from "react-icons/ri";
 
 import { useCallback, useEffect, useState } from "react";
 import { ServiceProp } from "../types";
@@ -11,6 +14,7 @@ import ListingCard from "./inputs/ListingCard";
 import Banner from "./navbar/Banner";
 import { useBooking } from "@/providers/BookingProvider";
 import { useSearchParams } from "next/navigation";
+import useFormAttendance from "../hooks/useFormAttendance";
 
 interface MainPageClientProps {
   services: ServiceProp[];
@@ -20,17 +24,103 @@ const MainPageClient: React.FC<MainPageClientProps> = ({ services }) => {
   // const services = await getServices();
   // const [servicesBooked, setServicesBooked] = useState<ServiceProp[]>([]);
 
+  const formAttendance = useFormAttendance();
+
   const params = useSearchParams();
+  // console.log(params);
 
   useEffect(() => {
-    // Get the value of PaymentStatus from the URL
     if (params) {
-      // Get the value of PaymentStatus from the URL
-      const paymentStatus = (params as any).PaymentStatus;
+      const paymentStatus = params.get("PaymentStatus");
 
       if (paymentStatus) {
-        // Do something with the PaymentStatus value
-        console.log("PaymentStatus:", paymentStatus);
+        if (paymentStatus === "00") {
+          toast.custom((t) => (
+            <div
+              className={`${
+                t.visible ? "animate-enter" : "animate-leave"
+              } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+            >
+              <div className="flex-1 w-0 p-4 cursor-pointer">
+                <div
+                  onClick={formAttendance.onOpen}
+                  className="flex items-start"
+                >
+                  <div className="flex-shrink-0 pt-0.5">
+                    <Image
+                      className="h-10 w-10 rounded-full"
+                      src="/images/logo-primary.svg"
+                      width={10}
+                      height={10}
+                      alt="logo-primary"
+                    />
+                  </div>
+                  <div className="ml-3 flex-1">
+                    <p className="flex flex-row items-center gap-1 text-md font-bold text-[#4f4fef]">
+                      Thanks for your payment!{" "}
+                      <GoHeartFill size={30} style={{ color: "red" }} />
+                    </p>
+                    <p className="mt-1 text-sm font-semibold text-gray-500">
+                      Your payment is successful at , you can create own your
+                      attendance schedule
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex border-l border-gray-200">
+                <button
+                  onClick={() => toast.dismiss(t.id)}
+                  className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          ));
+        } else if (paymentStatus === "10") {
+          toast.custom((t) => (
+            <div
+              className={`${
+                t.visible ? "animate-enter" : "animate-leave"
+              } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+            >
+              <div className="flex-1 w-0 p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0 pt-0.5">
+                    <Image
+                      className="h-10 w-10 rounded-full"
+                      src="/images/logo-primary.svg"
+                      width={10}
+                      height={10}
+                      alt="logo-primary"
+                    />
+                  </div>
+                  <div className="ml-3 flex-1">
+                    <p className="flex flex-row items-center gap-1 text-sm font-medium text-red-600">
+                      Payment failed!{" "}
+                      <RiEmotionSadLine
+                        size={30}
+                        style={{ color: "#ff6347" }}
+                      />
+                    </p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      Ohh, your payment is failed, please try again and check
+                      your information or contact us for more information
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex border-l border-gray-200">
+                <button
+                  onClick={() => toast.dismiss(t.id)}
+                  className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          ));
+        }
       }
     }
   }, [params]);
